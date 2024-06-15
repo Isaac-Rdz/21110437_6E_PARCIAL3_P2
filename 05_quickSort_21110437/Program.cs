06fae632529f0e43e7d41662532bf256e5b9ed6b
Isaac-Rdz <160627381+Isaac-Rdz@users.noreply.github.com>
2024-06-14 19:35:02 -0600

    Add files via upload

---
+++ 05_quickSort_21110437/Program.cs
@@ -0,0 +1,69 @@
+﻿// See https://aka.ms/new-console-template for more information
+//21110437
+//EMMANUEL ISAAC RODRIGUEZ MENDEZ
+//EJERCICIOS DE PRACTICA 2 Métodos de Ordenamiento.
+//6E_ING_MECATRONICA
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] arr = { 5, 2, 9, 1, 5, 6 };
+        Console.WriteLine("Arreglo original:");
+        PrintArray(arr);
+
+        QuickSort(arr, 0, arr.Length - 1);
+
+        Console.WriteLine("\nArreglo ordenado:");
+        PrintArray(arr);
+    }
+
+    // Función principal de Quicksort
+    static void QuickSort(int[] arr, int left, int right)
+    {
+        if (left < right)
+        {
+            int pivotIndex = Partition(arr, left, right);
+            QuickSort(arr, left, pivotIndex - 1);
+            QuickSort(arr, pivotIndex + 1, right);
+        }
+    }
+
+    // Función de partición
+    static int Partition(int[] arr, int left, int right)
+    {
+        int pivot = arr[right];
+        int i = left - 1;
+
+        for (int j = left; j < right; j++)
+        {
+            if (arr[j] <= pivot)
+            {
+                i++;
+                Swap(ref arr[i], ref arr[j]);
+            }
+        }
+
+        Swap(ref arr[i + 1], ref arr[right]);
+        return i + 1;
+    }
+
+    // Función para intercambiar elementos
+    static void Swap(ref int a, ref int b)
+    {
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+
+    // Función para imprimir el arreglo
+    static void PrintArray(int[] arr)
+    {
+        foreach (var item in arr)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
+    }
+}
